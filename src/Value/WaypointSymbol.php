7caac9c699aa5b445f2d111d4546b226dc4bab4e
Oscar Merida <oscar@oscarm.org>
2024-05-05 23:06:08 -0400

    Adds a waypoint symbol value object.

---
+++ src/Value/WaypointSymbol.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Phparch\SpaceTraders\Value;
+
+class WaypointSymbol
+{
+    public readonly string $sector;
+    public readonly string $system;
+    public readonly string $waypoint;
+
+    /** @see https://github.com/SpaceTradersAPI/api-docs/blob/main/models/WaypointSymbol.json */
+    public function __construct(string $input) {
+        preg_match('/^([[:alnum:]]{,2})\-([[:alnum:]]{,5})\-([[:alnum:]]{,5})$/', $input, $match);
+
+        if (!$match) {
+            throw new \InvalidArgumentException("Malformed waypoint string");
+        }
+
+        $this->sector = $match[1];
+        $this->system = $match[1] . '-' . $match[2];
+        $this->waypoint = $match[1] . '-' . $match[2] . '-' . $match[3];
+    }
+}
