e323d19344b39e24c0b48a22acf5076e5a87c608
Oscar Merida <oscar@oscarm.org>
2024-05-05 23:17:53 -0400

    Adds a system symbol value object.

---
+++ src/Value/SystemSymbol.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Phparch\SpaceTraders\Value;
+
+class SystemSymbol
+{
+    public readonly string $sector;
+    public readonly string $system;
+
+    /** @see https://github.com/SpaceTradersAPI/api-docs/blob/main/models/SystemSymbol.json */
+    public function __construct(string $input) {
+        preg_match('/^([[:alnum:]]{,2})\-([[:alnum:]]{,5})$/', $input, $match);
+
+        if (!$match) {
+            throw new \InvalidArgumentException("Malformed system string");
+        }
+
+        $this->sector = $match[1];
+        $this->system = $match[1] . '-' . $match[2];
+    }
+}
