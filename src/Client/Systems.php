23512c9b4f5adceadddee99eb0142f9c6b718573
Oscar Merida <oscar@oscarm.org>
2024-08-20 23:18:30 -0400

    Add a command to get a list of waypoints by type in a system.

--- src/Client/Systems.php
+++ src/Client/Systems.php
@@ -12,6 +12,16 @@ class Systems extends \Phparch\SpaceTraders\Client
             sprintf('systems/%s/waypoints/%s', $system, $waypoint)
         );
 
+        return $this->convertResponse($response, Response\Systems\Waypoint::class);
+    }
+
+    public function waypoints(string $system, string $type = '')
+    {
+        $url = sprintf('systems/%s/waypoints', $system);
+        if ($type) {
+            $url .= '?' . http_build_query(['type' => $type]);
+        }
+        $response = $this->get($url);
         return $this->convertResponse($response, Response\Systems\Waypoints::class);
     }
 }
