d749b882f67a34f61d5c744f06e982228e6ade4e
Oscar Merida <oscar@oscarm.org>
2026-04-03 00:31:13 -0400

    Marking services config as an example but its likely deprecated code.

--- config/services.php
+++ config/services.php
@@ -9,6 +9,7 @@ use Phparch\SpaceTraders\ServiceContainer;
 use Phparch\SpaceTraders\TwigExtensions;
 use Symfony\Component\Cache\Adapter\RedisAdapter;
 
+/** Sample config for your dependency injection container */
 return [
     Predis\Client::class => static function () {
         return new Predis\Client($_ENV['REDIS_URI']);
@@ -28,50 +29,4 @@ return [
         $stack->push(new CacheMiddleware($strategy), 'cache');
         return new GuzzleHttp\Client(['handler' => $stack]);
     },
-    Routes\Scanner::class => static function () {
-        return new Routes\Scanner(
-            controllerDirs: [
-                [
-                    'namespace' => 'Phparch\\SpaceTraders',
-                    'path' => dirname(__DIR__) . '/src/Controller/'
-                ]
-            ],
-            ref: ServiceContainer::get(
-                \Roave\BetterReflection\BetterReflection::class
-            ),
-            useAPCu: $_ENV['USE_APCU'] === 1,
-        );
-    },
-    Routes\Mapper::class => static function () {
-        return new SpaceTraders\Routes\Mapper(
-            scanner: ServiceContainer::get(Routes\Scanner::class),
-            registry: ServiceContainer::get(Routes\Registry::class),
-        );
-    },
-    Routes\Registry::class => static function () {
-        return new Routes\Registry(
-            container: ServiceContainer::instance(),
-            router: ServiceContainer::get(League\Route\Router::class),
-            decorator: ServiceContainer::get(Routes\Decorator::class)
-        );
-
-    },
-    Twig\Environment::class => static function () {
-        $twig = new Twig\Environment(
-            new \Twig\Loader\FilesystemLoader(dirname(__DIR__) . '/templates/'),
-            [
-                'debug' => $_ENV['TWIG_DEBUG'] ?? false,
-                'cache' => dirname(__DIR__) . '/templates_cache/',
-                'auto_reload' => $_ENV['TWIG_AUTORELOAD'] ?? false,
-                'autoescape' => 'html'
-            ]
-        );
-        if ($twig->isDebug()) {
-            $twig->addExtension(new \Twig\Extension\DebugExtension());
-        }
-        $twig->addExtension(
-            new \Twig\Extension\AttributeExtension(TwigExtensions::class)
-        );
-        return $twig;
-    }
 ];
